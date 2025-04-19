501baa0c19ad336cb069ab5801e1fdb6a83bb6af
Rafael <43618377+HeyyCzer@users.noreply.github.com>
2025-04-19 12:51:08 -0300

    improve code quality

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,10 @@
-fx_version 'adamant'
-game 'gta5'
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
 
-author "HeyyCzer"
+author "HeyyCzer (heyyshop.com.br)"
+version "1.0.1"
+description "12 com bala de borracha"
 
 client_scripts {
    "config.lua",
--- src/client.lua
+++ src/client.lua
@@ -1,31 +1,27 @@
 local lessLethalWeapon = GetHashKey(config.weapon)
 local fallDownTime = config.secondsInGround
 
-Citizen.CreateThread(function()
-	while true do
-		local idle = 1000
-		local ped = PlayerPedId()
-		local armed, weaponHash = GetCurrentPedWeapon(ped)
-		if armed and weaponHash == lessLethalWeapon then
-			idle = 0
-			SetWeaponDamageModifierThisFrame(weaponHash, 0.1)
-		end
-		Citizen.Wait(idle)
-	end
+CreateThread(function()
+	SetWeaponDamageModifier(weaponHash, 0.1)
 end)
 
 AddEventHandler('gameEventTriggered', function (name, args)
+	if name ~= "CEventNetworkEntityDamage" then return end
+	
 	local ped = PlayerPedId()
-	if name == "CEventNetworkEntityDamage" and args[1] == ped then
-		if args[7] == lessLethalWeapon then
-			CancelEvent()
-			
-			local seconds = fallDownTime
-			while seconds > 0 do
-				SetPedToRagdollWithFall(ped,1500,1500,0,GetEntityForwardVector(args[2]),10.0,0.0,0.0,0.0,0.0,0.0,0.0)
-				seconds = seconds - 1
-				Citizen.Wait(1000)
-			end
-		end
+
+	local victim, weapon = args[1], args[7]
+	if victim ~= ped or weapon ~= lessLethalWeapon then return end
+
+	CancelEvent()
+	
+	local ragdoll = true
+	while ragdoll do
+		SetPedToRagdollWithFall(ped,1500,1500,0,GetEntityForwardVector(args[2]),10.0,0.0,0.0,0.0,0.0,0.0,0.0)
+		Wait(0)
 	end
+
+	SetTimeout(fallDownTime * 1000, function()
+		ragdoll = nil
+	end)
 end)
